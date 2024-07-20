b06e7a392a443d78a5115593162913acc0fecb88
rupesh <rupesh@gmail.com>
2024-07-20 16:56:44 +0530

    first

--- app.py
+++ app.py
@@ -1,2 +1,17 @@
 def calculator():
-    pass
+    x=int(input("enter a number"))
+    z=int(input("enter a number"))
+    print("press 1 for (+)")
+    print("press 2 for (-)")
+    print("press 3 for (/)")
+    print("press 4 for (*)")
+    a=int(input("enter your choice"))
+    if(a==1):
+        x+z
+    elif(a==2):
+        x-z
+    elif(a==3):
+        x/z
+    else:
+        x*z
+calculator()
--- db.py
+++ db.py
@@ -1,2 +1,11 @@
+import mysql.connector
 def db():
-    pass
+    database= mysql.connector.connect(host="localhost",user="root",password="000001")
+    box=database.cursor()   
+    try:
+        box.execute("creat database xyz")
+        box.execute("use xyz")
+        box.execute("insert into xyz")
+
+    
+            
