c5c51d5635ef75637460c7947640904cd94adb77
shivangi <shivangi.bansal2310@gmail.com>
2024-07-20 17:38:47 +0530

    first

--- app.py
+++ app.py
@@ -1,2 +1,25 @@
 def calculator():
-    pass
+    a = int(input("enter 1st number:"))
+    b = int(input("enter 2nd number:"))
+    print("select a choice:")
+    print("1.Addition")
+    print("2.Subtraction")
+    print("3.Multiplication")
+    print("4.Division")
+    choice=int(input("select a choice:"))
+    if(choice==1):
+        print(a+b)
+
+    elif(choice==2):
+        print(a-b)
+
+    elif(choice==3):
+        print(a*b)
+    
+    elif(choice==4):
+        print(a/b)
+
+    else :
+        print("invalid choice")
+
+calculator()
--- db.py
+++ db.py
@@ -1,2 +1,23 @@
-def db():
-    pass
+import mysql.connector
+def db(n1,n2,sym,res):
+    data = mysql.connector.connect(host="localhost",user="root",password="7011581376")
+    box = data.cursor()
+    try:
+      box.execute("create database shiv")
+      box.execute("use shiv")
+    except:
+       box.execute("use shiv")
+    try:
+       box.execute("create table skb(num1 int, num2 int, choice int, result int)")
+    except:
+       print("table already exists")
+    q = "insert into skb values(%s,%s,%s,%s)"
+    val=(n1,n2,sym,res)
+    box.execute(q,val)
+    data.commit()
+    data.close()
+    print("data added")
+      
+      
+
+
