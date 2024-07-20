5433c20ceb8ce0ca1ae226b5e7c6eb24b346779f
nitin <nitinshrivastav3@gmail.com>
2024-07-20 16:35:57 +0530

    first

--- db.py
+++ db.py
@@ -1,2 +1,20 @@
-def db():
-    pass
+import mysql.connector
+def db(n1,n2,sym,res):
+    
+    database = mysql.connector.connect(host='localhost',user='root',password='1234567890')
+    box=database.cursor()
+    try:
+        box.execute('create database abc')
+        box.execute('use abc')
+    except:
+        box.execute('use abc')
+    try:
+         box.execute('create table calculation(num1 int,num2 int,choice varchar(1),ans int)')
+    except:
+        print('table is already created')
+    q='insert into calculation values(%s,%s,%s,%s)'
+    data=(n1,n2,sym,res)
+    box.execute(q,data)
+    database.commit()
+    database.close()        
+    print("Data has been added")
